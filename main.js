3aa42f74336ea43950379111ec33631f6e564dd6
dayoungin-ucsd <dain@ucsd.edu>
2026-05-12 04:34:01 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,11 +12,31 @@ const path = d3.geoPath();
 
 d3.json("https://d3js.org/us-10m.v2.json").then(us => {
   svg.append("g")
-      .attr("class", "states")    
-      .selectAll("path")
-      .data(feature(us, us.objects.states).features)
-      .enter().append("path")
-      .attr("d", path)
-      .attr("fill", "#ccc")
-      .attr("stroke", "#fff");
+    .attr("class", "states")
+    .selectAll("path")
+    .data(feature(us, us.objects.states).features)
+    .enter().append("path")
+    .attr("d", path)
+    .attr("fill", "#ccc")
+    .attr("stroke", "#fff")
+    .on("mouseover", function () {
+        d3.select(this)
+          .attr("fill", "#128127");
+
+        tooltip.style("display", "block")
+               .style("left", `${event.pageX + 10}px`)
+               .style("top", `${event.pageY + 10}px`)
+               .html(`
+                   <strong>${d.properties.name}</strong><br>
+                   Risk Score: 72
+                `);
+    })
+    .on("mouseout", function() {
+        d3.select(this)
+          .attr("fill", "#ccc");
+
+        tooltip.style("display", "none");
+    });
 });
+
+const tooltip = d3.select("#tooltip");
