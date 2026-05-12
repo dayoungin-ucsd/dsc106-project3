079d4c822652c667a0d0575a5dc8f05dd4aa7bd9
dayoungin-ucsd <dain@ucsd.edu>
2026-05-12 04:25:42 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,18 +3,19 @@ import { feature } from 'https://cdn.jsdelivr.net/npm/topojson-client@3/+esm';
 
 const width = 960, height = 600;
 
-const svg = d3.select("body").append("svg")
+const svg = d3.select("#vis").append("svg")
     .attr("width", width)
     .attr("height", height);
+    .attr("viewBox", `0 0 ${width} ${height}`);
 
 const path = d3.geoPath();
 
 d3.json("https://d3js.org/us-10m.v2.json").then(us => {
   svg.append("g")
-      .attr("class", "states")
-    .selectAll("path")
-    .data(topojson.feature(us, us.objects.states).features)
-    .enter().append("path")
+      .attr("class", "states")    
+      .selectAll("path")
+      .data(feature(us, us.objects.states).features)
+      .enter().append("path")
       .attr("d", path)
       .attr("fill", "#ccc")
       .attr("stroke", "#fff");
