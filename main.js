26460a600091c4ff9936b8aeae7ed179ab05ff55
dayoungin-ucsd <dain@ucsd.edu>
2026-05-12 04:22:47 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+const width = 960, height = 600;
+
+const svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+const path = d3.geoPath();
+
+d3.json("https://d3js.org/us-10m.v2.json").then(us => {
+  svg.append("g")
+      .attr("class", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr("d", path)
+      .attr("fill", "#ccc")
+      .attr("stroke", "#fff");
+});
